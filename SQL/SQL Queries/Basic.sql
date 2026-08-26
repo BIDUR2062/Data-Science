0ccd96f932ce4fabd527b6d8556177276fb59717
Bidur Khanal <bidurkhanal111@gmail.com>
2026-08-26 16:15:40 +0545

    Add Time Analysis

--- SQL/SQL Queries/Basic.sql
+++ SQL/SQL Queries/Basic.sql
@@ -124,6 +124,47 @@ SELECT first_name,
 FROM   sales.customers;
 
 --find this pattern customer_id-first-name(3,2)-last_name(last 4)- email(5,6) - street(frist 2) - city(2,3) - state - zip_code(2,3)
-SELECT CONCAT(customer_id, '-', SUBSTRING(first_name, 3, 2), '-', RIGHT(last_name, 4), '-', SUBSTRING(email, 5, 6), '-', LEFT(street, 2), SUBSTRING(city, 2, 3), '-', state, '-', SUBSTRING(zip_code, 2, 3)) AS pin
+SELECT CONCAT(customer_id, '-', SUBSTRING(first_name, 3, 2), '-', RIGHT(last_name, 4), '-', SUBSTRING(email, 5, 6), '-', LEFT(street, 2), SUBSTRING(city, 2, 3), '-', state, '-', SUBSTRING(zip_code, 2, 3)) AS cust_id,
+       first_name,
+       last_name,
+       email,
+       street,
+       city,
+       state,
+       zip_code
 FROM   sales.customers;
 
+--Difference between CONCAT and + Operator Query
+SELECT CONCAT(first_name, phone) AS data,
+       (first_name + phone) AS data1
+FROM   sales.customers;
+
+--Time Series Analysis / Cohort Analyisis
+SELECT order_date,
+       year(order_date) AS O_Year,
+       month(order_date) AS O_Month,
+       day(order_date) AS O_day,
+       datename(month, order_date) AS Order_Month,
+       datename(weekday, order_date) AS Order_day,
+       datepart(weekday, order_date) AS dp_day,
+       datepart(week, order_date) AS dp_week,
+       datepart(quarter, order_date) AS dp_qt,
+       format(order_date, 'MM') AS order_month, --MMMM->Full_name MMM-> Half_name(jan) MM and M
+       format(order_date, 'dddd') AS order_day
+FROM   sales.orders;
+
+--Date Analysis
+SELECT order_date,shipped_date,
+datediff(day,order_date,isnull(shipped_date,getdate())) as Day_Diff,
+datediff(month,order_date,shipped_date) as Month_diff,
+dateadd(day,3,order_date) as add_day
+FROM   sales.orders;
+
+--For Temporary Filling 
+--ISNull
+--Coalesce
+SELECT shipped_date,
+       IsNull(shipped_date, getdate()),
+       COALESCE (shipped_date, getdate())
+FROM   sales.orders;
+-------------------------------
