76344a93f9f28df77061dfdad0f3e0c0e702a4f4
Bidur Khanal <bidurkhanal111@gmail.com>
2026-09-03 15:48:14 +0545

    Complete Sub Query

--- SQL/SQL Queries/SubQuery.sql
+++ SQL/SQL Queries/SubQuery.sql
@@ -87,11 +87,66 @@ SELECT concat(sc.first_name, ' ', sc.last_name) AS Customer_name,
        sc.state,
        sc.zip_code
 FROM   sales.customers AS sc
-WHERE  customer_id IN (SELECT top 5 so.customer_id
-                       FROM   sales.order_items AS soi
-                              INNER JOIN
-                              sales.orders AS so
-                              ON soi.order_id = so.order_id
-                       WHERE  ((soi.list_price * soi.quantity) * (1 - soi.discount)) > 8000
-                              AND order_status = 4
-                              order by (soi.list_price * soi.quantity) * (1 - soi.discount) desc);
+WHERE  customer_id IN (SELECT   TOP 5 so.customer_id
+                       FROM     sales.order_items AS soi
+                                INNER JOIN
+                                sales.orders AS so
+                                ON soi.order_id = so.order_id
+                       WHERE    ((soi.list_price * soi.quantity) * (1 - soi.discount)) > 8000
+                                AND order_status = 4
+                       ORDER BY (soi.list_price * soi.quantity) * (1 - soi.discount) DESC);
+
+/*
+ANY-> OR
+ALL ->  AND
+*/
+SELECT   *
+FROM     production.products
+ORDER BY list_price ASC;
+
+--Find any product whose list_price is less than 209.99/ 250.99
+SELECT *
+FROM   production.products
+WHERE  list_price < ANY (SELECT list_price
+                         FROM   production.products
+                         WHERE  list_price IN (209.99, 250.99));
+
+--Find any product whose list_price is less than 209.99/ 250.99
+SELECT *
+FROM   production.products
+WHERE  list_price < ALL (SELECT list_price
+                         FROM   production.products
+                         WHERE  list_price IN (209.99, 250.99));
+
+--Find Customer details who brought the product whose list_price is less than 209.99 / 250.99
+SELECT first_name,
+       last_name,
+       email,
+       zip_code
+FROM   sales.customers
+WHERE  customer_id IN (SELECT customer_id
+                       FROM   sales.orders
+                       WHERE  order_id IN (SELECT order_id
+                                           FROM   sales.order_items
+                                           WHERE  product_id IN (SELECT product_id
+                                                                 FROM   production.products
+                                                                 WHERE  list_price < ALL (SELECT list_price
+                                                                                          FROM   production.products
+                                                                                          WHERE  list_price IN (209.99, 250.99)))));
+
+--Find staff details who have managed orders which was rejected and sold products whose price is more than 1000
+--whose model year is 2017
+SELECT first_name,
+       last_name,
+       email,
+       phone
+FROM   sales.staffs
+WHERE  staff_id IN (SELECT staff_id
+                    FROM   sales.orders
+                    WHERE  order_status = 3
+                           AND order_id IN (SELECT order_id
+                                            FROM   sales.order_items
+                                            WHERE  list_price > 1000
+                                                   AND product_id IN (SELECT product_id
+                                                                      FROM   production.products
+                                                                      WHERE  model_year = 2017)));
